3448984c848e8822372b7ec6f4ab6d0eb958a278
dyg <dyg1993@foxmail.com>
2015-10-23 17:22:50 +0800

    just a test

---
+++ test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: laoding
+	> Mail: dyg1993@foxmail.com 
+	> Created Time: Fri 23 Oct 2015 05:21:54 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("just test!\n");
+	retrurn 0;
+}
